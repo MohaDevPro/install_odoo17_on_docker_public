4a836693024be631b91ace29f51995f084b79c25
M <DevITPro@protonmail.com>
2024-09-25 22:26:32 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,12 +6,12 @@ PGADMIN=$4
 ODOO_VERSION=${5:-17}  # Set default Odoo version to 17 if not provided
 
 # Clone Odoo directory
-git clone --depth=1 https://github.com/MohaDevPro/install_odoo17_on_docker_public.git $DESTINATION
-rm -rf $DESTINATION/.git
+git clone --depth=1 https://github.com/MohaDevPro/install_odoo17_on_docker_public.git "$DESTINATION"
+rm -rf "$DESTINATION/.git"
 
 # Set permissions
-mkdir -p $DESTINATION/postgresql
-sudo chmod -R 777 $DESTINATION
+mkdir -p "$DESTINATION/postgresql"
+sudo chmod -R 777 "$DESTINATION"
 
 # Config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then
@@ -22,15 +22,15 @@ fi
 sudo sysctl -p
 
 # Update ports in docker-compose.yml
-sed -i "s/10015/$PORT/g" $DESTINATION/docker-compose.yml
-sed -i "s/20015/$CHAT/g" $DESTINATION/docker-compose.yml
-sed -i "s/5053/$PGADMIN/g" $DESTINATION/docker-compose.yml
+sed -i "s/10015/$PORT/g" "$DESTINATION/docker-compose.yml"
+sed -i "s/20015/$CHAT/g" "$DESTINATION/docker-compose.yml"
+sed -i "s/5053/$PGADMIN/g" "$DESTINATION/docker-compose.yml"
 
 # Update Odoo version in image and replace 'odoo17' with 'odoo' + the provided version
-sed -i "s|odoo:[0-9]\+|odoo:$ODOO_VERSION|g" $DESTINATION/docker-compose.yml
-sed -i "s|odoo17|odoo$ODOO_VERSION|g" $DESTINATION/docker-compose.yml
+sed -i "s|odoo:[0-9]\+|odoo:$ODOO_VERSION|g" "$DESTINATION/docker-compose.yml"
+sed -i "s|odoo17|odoo$ODOO_VERSION|g" "$DESTINATION/docker-compose.yml"
 
 # Run Odoo
-docker compose -f $DESTINATION/docker-compose.yml up -d
+docker compose -f "$DESTINATION/docker-compose.yml" up -d
 
 echo "Started Odoo @ http://localhost:$PORT | Master Password: admin@1234 | Live chat port: $CHAT"
