5c34cb1a60dd4c2129174175a43110aa7be2367c
M <DevITPro@protonmail.com>
2024-10-11 23:50:09 +0300

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,7 @@ set -e
 : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
 : ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
 : ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo17ss'}}}
-apt-get update -y
-apt-get upgrade -y
+
 # install python packages
 pip3 install pip --upgrade
 
